78bddcc35bf32040fae4e964b67bfb1a585ef9b9
LiuJinChuan <1825290052@qq.com>
2017-12-22 08:47:42 +0800

    two

---
+++ js/click.js
@@ -0,0 +1,6 @@
+function dianwo(){
+    var dtxt = document.getElementById("text");
+    var dmus = document.getElementById("music");
+    dtxt.style.display="none";
+    dmus.style.display="block";
+}
