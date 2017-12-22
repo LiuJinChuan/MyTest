791099be667c3b7e30a2aaa220d0a82f672834b7
LiuJinChuan <1825290052@qq.com>
2017-12-22 10:01:52 +0800

    6

--- js/click.js
+++ js/click.js
@@ -1,9 +1,3 @@
-function dianwo(){
-    var dtxt = document.getElementById("text");
-    var dmus = document.getElementById("music");
-    dtxt.style.display="none";
-    dmus.style.display="block";
-}
 function bofang(){
     var bwod = document.getElementById("wodian");
     var bvoi = document.getElementById("voice");
